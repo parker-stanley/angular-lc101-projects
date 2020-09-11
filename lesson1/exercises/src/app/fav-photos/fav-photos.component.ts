78029814d1b241104f3e13212fed00d416fe6ce0
Parker Stanley <parker.stanley@SOMO-M-DC-005.local>
2020-09-10 21:00:22 -0400

    completed exercise up Part 2 - adding more components

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'Everyone tells me these are the best photos';
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://crhscountyline.com/wp-content/uploads/2020/03/Capture.png';
+  image3 = 'https://upload.wikimedia.org/wikipedia/en/f/ff/SuccessKid.jpg';
 
   constructor() { }
 
