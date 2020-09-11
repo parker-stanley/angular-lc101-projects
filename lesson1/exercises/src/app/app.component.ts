06198a570dc897d86154202fde39c966e37709e7
Parker Stanley <parker.stanley@SOMO-M-DC-005.local>
2020-09-10 21:31:17 -0400

    finished all bonus missions

--- lesson1/exercises/src/app/app.component.ts
+++ lesson1/exercises/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-
+  gif: string = "https://media.giphy.com/media/dlMIwDQAxXn1K/giphy.gif";
 }
